ddf1de64757bba4d310a3b719f579f8282ec125d
Victor Cunha <cunhavictorr@gmail.com>
2021-08-03 03:26:20 -0300

    Updating readme and etl dag with telegram bot

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# pff2_price_automation
-A gathering data of pff2 masks project using selenium, airflow and sqlitedb.
+**Table of Contents**
+
+[TOCM]
+
+[TOC]
+
+# Automação da busca de preço de máscaras PFF2
+Program for Aura 3M mask pricing and availability verification.
+
+# About The Project
+
+The purpose of this project was to become familiar with Apache Airflow. My motivation was by the fact that, during the pandemic, the 3m brand PFF2 masks quickly sold out, so I developed this script, which searches the website of the store. As a result of the script, the date, the name, the price, the status, and the link to the product are returned. A SQLite database is used to store the information. Finally, if the product is available, a Telegram bot will send the user an alert with a link to purchase.
+
+## Built With
+-------------
+
+- Language
+
+    ![Badge](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=darkgreen)
+
+- Frameworks
+
+    ![Badge](https://img.shields.io/badge/Pandas-2C2D72?style=for-the-badge&logo=pandas&logoColor=white)
+    ![Badge](https://img.shields.io/badge/Selenium-43B02A?style=for-the-badge&logo=Selenium&logoColor=white)
+
+- Database system
+
+    ![Badge](https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=white	)
+
+
+- Workflow managment
+
+    ![Badge](https://img.shields.io/badge/Airflow-017CEE?style=for-the-badge&logo=Apache%20Airflow&logoColor=white)
+
+- IDEs
+
+    ![Badge](https://img.shields.io/badge/Jupyter-F37626.svg?&style=for-the-badge&logo=Jupyter&logoColor=white)
+    ![Badge](https://img.shields.io/badge/VSCode-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white)
+
+<br>
+
+
+# Getting Started
+
+The following steps demonstrate how to run this locally.
+
+## Prerequisites    
+- Python 3.7+
+
+- Virtual Enviroment ***(Optinal)***
+
+    `python -m venv venv`
+
+## Installation
+
+1. Clone the repository
+
+        git clone https://github.com/vrcunha/pff2_price_automation.git
+
+2. Install the requirements.txt
+
+       pip install -r requirements.txt
+ 
+3. Start the airflow db
+
+        airflow db init
+
+4. Create a user (Replace bracket with your name) 
+
+        airflow users create --username admin --password 123admin --firstname [fname] --lastname [lname] --role Admin --email [email]
+
+## Usage
+
+1. Start the webserver
+
+        airflow webserver
+
+2. Start the scheduler
+
+        airflow scheduler
+
+3. Then, you need to put the dag in the airflow dags folder
+    > PS: For the first time, you will need to create a folder inside airflow called dags.
+
+4. The token and chat ID must be included in the DAG in order for the telegram bot to work.
+
+# Contribuições
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are greatly appreciated.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+# Autor
+[![Badge](https://img.shields.io/badge/Autor-Victor_Cunha-07405E?style=for-the-badge)](https://github.com/vrcunha/)
+
+#### Contatos
+
+[![Badge](https://img.shields.io/badge/Github-black?style=for-the-badge&logo=github)](https://github.com/vrcunha)
+[![Badge](https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin)](https://www.linkedin.com/in/victor-de-rezende-cunha)
+[![Badge](https://img.shields.io/badge/Telegram-blue?style=for-the-badge&logo=telegram)](https://t.me/VictorRCunha)
+[![Badge](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](mailto:cunhavictorr@gmail.com)
+
+# Licença
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ pff_etl_pipeline.py
@@ -0,0 +1,126 @@
+import os
+import os.path
+import telegram
+from dotenv import load_dotenv
+import sqlite3
+import pandas as pd
+from datetime import datetime as dt
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from selenium.webdriver import Firefox
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
+
+default_args = {'owner': 'Victor'}
+
+path = "/Users/victorcunha/Codes/pff2_dl_airflow/"
+path_db = path+"pff_mask.db"
+path_env = path+'.env'
+path_temp_csv = path+"_temp.csv"
+
+load_dotenv(path_env)
+
+dag = DAG(
+    dag_id='etl_pff2_telegram',
+    default_args=default_args,
+    schedule_interval='*/10 * * * *',
+    start_date=dt(2021, 8, 2),
+    tags=['ETL_BOT'],
+)
+
+def find_aura_3m(browser, BASE_URL):
+    browser.get(BASE_URL)
+    try:
+        name_div = browser.find_element_by_css_selector('div.name-product')
+        name = name_div.text
+    except NoSuchElementException:
+        name = None
+    try:
+        link = name_div.find_element_by_css_selector('a')
+        link = link.get_attribute('href')
+    except Exception as e:
+        link = None
+    try:
+        price_span = browser.find_element_by_css_selector('span.best-price')
+        price = price_span.text
+        status = 'produto disponível'
+    except NoSuchElementException:
+        no_price_span = browser.find_element_by_css_selector('.bt-product-unavailable')
+        price = 0
+        status = no_price_span.text.lower()
+    return name, link, price, status
+
+def _extract():
+    opt = Options()
+    opt.headless = True
+    date = dt.now().strftime('%Y-%m-%d')
+    BASE_URL = 'https://www.loja3m.com.br/busca?ft=aura%209320'
+
+    browser = Firefox(options=opt)
+    name, link, price, status = find_aura_3m(browser, BASE_URL)
+    browser.quit()
+
+
+    with open(path_temp_csv, 'w') as f:
+        f.write(f'date;name;price;status;link\n')
+        f.write(f'{date};{name};{price};{status};{link}\n')
+
+def _transform():
+    dataset_df = pd.read_csv(path_temp_csv, sep=';')
+
+    try:
+        if 'R$' in dataset_df.iloc[0,2]:
+            dataset_df.iloc[0,2] = dataset_df.iloc[0,2].replace('R$', '').strip()
+        if ',' and '.' in dataset_df.iloc[0,2]:
+            dataset_df.iloc[0,2] = dataset_df.iloc[0,2].replace('.', '').replace(',', '.')
+        dataset_df.price = pd.to_numeric(dataset_df.price, downcast="float").map('{:,.2f}'.format)
+    except TypeError:
+        dataset_df.price = pd.to_numeric(dataset_df.price, downcast="float").map('{:,.2f}'.format)
+    except AttributeError:
+        dataset_df.price = pd.to_numeric(dataset_df.price, downcast="float").map('{:,.2f}'.format)
+
+    dataset_df.status.replace({'produto disponível':1, 'produto indisponível':0}, inplace=True)
+
+    dataset_df.to_csv(
+        path_temp_csv,
+        index=False
+    )
+
+def _load():
+    connect_db = sqlite3.connect(path_db)
+    
+    dataset_df = pd.read_csv(path_temp_csv)
+
+    dataset_df.to_sql("pff_masks", connect_db, if_exists="append", index=False)
+
+def telegram_msg(chat, token):
+    df = pd.read_csv(path_temp_csv)
+    if df.status.values != [0]:
+        msg = f'O produto {df.iloc[0,1]} -> Disponível\n{df.iloc[0,4]}'
+    bot = telegram.Bot(token=token)
+    bot.sendMessage(chat_id=chat, text=msg)
+
+extract_task = PythonOperator(
+    task_id="extract", python_callable=_extract, dag=dag
+)
+
+transform_task = PythonOperator(
+    task_id="transform", python_callable=_transform, dag=dag
+)
+
+load_task = PythonOperator(
+    task_id="load", python_callable=_load, dag=dag
+)
+
+telegram_task = PythonOperator(
+    task_id="telegram",
+    op_args=[
+        os.getenv('TELEGRAM_CHAT_ID'),
+        os.getenv('TELEGRAM_TOKEN')
+    ],
+    python_callable=telegram_msg,
+    dag=dag
+)
+
+extract_task >> transform_task >> load_task >> telegram_task
